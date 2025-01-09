9e2de1a56213fc35c5d518d3a9b7fede83c70480
VictorFil67 <victorfilinovych@gmail.com>
2025-01-09 09:34:26 +0200

    created deleteOnePost service

--- services/postsServices.ts
+++ services/postsServices.ts
@@ -8,3 +8,9 @@ export async function update(id: number, data: any) {
     data,
   });
 }
+
+export async function deleteOnePost(id: number) {
+  return await prisma.post.delete({
+    where: { id },
+  });
+}
