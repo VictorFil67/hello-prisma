98ff56d63fc1a07a5aeb87d9c1c99d2a0d661bd0
VictorFil67 <victorfilinovych@gmail.com>
2025-01-10 12:20:43 +0200

    created signup controller

---
+++ controllers/authControllers.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { register } from "../services/authServices";
+import ctrlWrapper from "../decorators/ctrlWrapper";
+
+const signup = async (req: Request, res: Response) => {
+  const { email, name, password, title, bio, content = "" } = req.body;
+  const result = await register({
+    // data: {
+    email,
+    name,
+    password,
+    posts: {
+      create: { title, content },
+    },
+    profile: {
+      create: { bio },
+    },
+    // },
+  });
+  res.status(201).json(result);
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+};
