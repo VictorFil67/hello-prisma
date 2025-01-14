82f242c66e9eb5ff27276cf5e754a0db63f27e23
VictorFil67 <victorfilinovych@gmail.com>
2025-01-14 21:01:25 +0200

    updated validateBody decorator

--- decorators/validateBody.ts
+++ decorators/validateBody.ts
@@ -1,21 +1,17 @@
 import { z } from "zod";
-import { NextFunction, Request } from "express";
+import { NextFunction, Request, Response } from "express";
 import HttpError from "../helpers/HttpError";
-
-// type Validator = (req: Request, next: NextFunction) => Promise<HttpError>;
 
 const validateBody = (schema: z.ZodSchema) => {
   //you need to pass the zod schema itself to validateBody not a type
-  const func = (req: Request, _: any, next: NextFunction) => {
-    // console.log(typeof req.body);
-    // const { error } = schema.parse(req.body);
+  const func = (req: Request, res: Response, next: NextFunction) => {
     try {
       const validatedBody = schema.parse(req.body);
       req.body = validatedBody;
       next();
     } catch (err) {
       if (err instanceof z.ZodError) {
-        console.log(err.errors);
+        // console.log(err.errors);
         return next(
           new HttpError(
             400,
@@ -27,7 +23,6 @@ const validateBody = (schema: z.ZodSchema) => {
       }
       next(err);
     }
-    // console.log("schema.parse(req.body): ", schema.parse(req.body));
   };
   return func;
 };
