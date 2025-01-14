19f52bc43aac4c4003a0fcd263fb0cfe8f99e226
VictorFil67 <victorfilinovych@gmail.com>
2025-01-14 21:33:56 +0200

    added zod validation for body in signip request

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -2,13 +2,13 @@ import express from "express";
 import authControllers from "../controllers/authControllers";
 import authenticate from "../middlewares/authenticate";
 import validateBody from "../decorators/validateBody";
-import { signupSchema } from "../Schemas/usersSchemas";
+import { signinSchema, signupSchema } from "../Schemas/usersSchemas";
 
 const authRouter = express.Router();
 
 const { signup, signin } = authControllers;
 
 authRouter.post("/", validateBody(signupSchema), signup);
-authRouter.post("/signin", signin);
+authRouter.post("/signin", validateBody(signinSchema), signin);
 
 export default authRouter;
